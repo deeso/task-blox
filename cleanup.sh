ce64dc4aaf673af9cee3c0c6590d8cb7d9bb5a09
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2018-01-15 13:42:24 -0600

    fixed some minor issues

--- cleanup.sh
+++ cleanup.sh
@@ -1,16 +1,18 @@
 PMOD="task-blox"
 VENV="venv"
-PROJECT_BASE="/research_data/code/git/"
+PROJECT_BASE="/research_data/code/infosec-concierge/"
 PROJECT=$PROJECT_BASE/$PMOD/
 
 VIRTUAL_ENV=$PROJECT/$VENV
-PYTHON=$VIRTUAL_ENV"/bin/python"
+PYTHON="/usr/bin/python3" #$VIRTUAL_ENV"/bin/python3"
+SITE_PACKAGES=$HOME/.local/lib/python3.5/site-packages/
 
 # cleanup local directory
-rm -r $VIRTUAL_ENV/dist-packages/* \
+rm -rf $VIRTUAL_ENV/dist-packages/* \
       $PROJECT/src/$PMOD.egg-info/ \
       $PROJECT/dist/ \
       $PROJECT/build/
+      $SITE_PACKAGES/task_blox* \
 
 rm -r $VIRTUAL_ENV
 
--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 PMOD="task-blox"
 VENV="venv"
-PROJECT_BASE="/research_data/code/git/"
+PROJECT_BASE="/research_data/code/infosec-concierge/"
 PROJECT=$PROJECT_BASE/$PMOD/
 
 VIRTUAL_ENV=$PROJECT/$VENV
-PYTHON=$VIRTUAL_ENV"/bin/python"
+PYTHON=/usr/bin/python3 #$VIRTUAL_ENV"/bin/python3"
+PIP=/usr/bin/pip3 #$VIRTUAL_ENV"/bin/pip3"
 
 bash cleanup.sh
 
 virtualenv -p /usr/bin/python3 $VIRTUAL_ENV
-$PYTHON $PROJECT_BASE/$SPOJ/setup.py install
-$PYTHON setup.py install
+$PIP install -r $PROJECT/requirements.txt .
+
 
